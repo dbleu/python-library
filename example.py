2cf69ab8cbc0e5ce9a0e1acee8c50c7c131bdea6
its_florent <70605487+itsflorent@users.noreply.github.com>
2022-02-17 11:52:57 +0100

    Update example.py

--- example.py
+++ example.py
@@ -5,16 +5,25 @@ from discord.ext import commands
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("-"), intents=intents)
 
+dbleuKEY = "" # Get it from https://dev.discord-botlist.eu
 
 @bot.event
 async def on_ready():
+    auto_post.start()
+    
     print(f"Online as {bot.user.name}")
+    
+    r = dbleu_getbotvotes(dbleuKEY, log_disable=False/True)
+    print(r.content)
 
-    dbleuKEY = "" # Get it from https://dev.discord-botlist.eu
+    r = dbleu_getbotdata(dbleuKEY, log_disable=False/True)
+    print(r.content)
 
-    dbleu_postservercount(dbleuKEY, bot, log_disable=False)
 
-
+@tasks.loop(minutes=5)
+async def auto_post():
+    dbleu_postservercount(dbleuKEY, bot, log_disable=False/True)
+    # IMPORTANT: The second parameter must be the discord.ext.commands.bot.Bot object
 
 
 bot.run("DISCORD-BOT-TOKEN")
